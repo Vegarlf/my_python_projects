aa79cd31d1e124159e7b4104dc6ee3357255bf0a
Vegarlf <dash.daivix.apps@gmail.com>
2026-03-06 21:12:00 +0530

    Added Utilities To card_engine, removed from its gitignore

---
+++ CLI_Games/card_engine/src/utils/caesar_cipher.py
@@ -0,0 +1,310 @@
+"""Caesar Cipher Encrypter, Decrypter and Brute Forcer.
+
+Most Of The Code Is Cloned From The Algorithms Github Repo, But With
+The brute_force_dict_search() Function Added
+To Search For Target Strings In The Brute Force Results.
+"""
+
+from __future__ import annotations
+
+from string import ascii_uppercase
+from string import digits
+
+"""
+Most Of The Code Is Self-Explanatory, But If You Need More Info,
+Please Read The Docstrings.
+Most Of The Code Is Cloned From The Encrypt Function, But With A Negative Key To Decrypt
+"""
+
+
+def encrypt(input_string: str, key: int, alphabet: str | None = None) -> str:  # noqa: D417
+    """Encrypt.
+
+    Encodes a given string with the caesar cipher and returns the encoded
+    message
+
+    Parameters
+    ----------
+    *   `input_string`: the plain-text that needs to be encoded
+    *   `key`: the number of letters to shift the message by
+
+    Optional:
+
+    *   `alphabet` (``None``): the alphabet used to encode the cipher, if not
+        specified, the standard english alphabet with upper and lowercase
+        letters is used
+
+    Returns
+    -------
+    *   A string containing the encoded cipher-text
+
+    More on the caesar cipher
+    =========================
+
+    The caesar cipher is named after Julius Caesar who used it when sending
+    secret military messages to his troops. This is a simple substitution cipher
+    where every character in the plain-text is shifted by a certain number known
+    as the "key" or "shift".
+
+    Example:
+    Say we have the following message:
+    ``Hello, captain``
+
+    And our alphabet is made up of lower and uppercase letters:
+    ``abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ``
+
+    And our shift is ``2``
+
+    We can then encode the message, one letter at a time. ``H`` would become ``J``,
+    since ``J`` is two letters away, and so on. If the shift is ever too large, or
+    our letter is at the end of the alphabet, we just start at the beginning
+    (``Z`` would shift to ``a`` then ``b`` and so on).
+
+    Our final message would be ``Jgnnq, ecrvckp``
+
+    Further reading
+    ===============
+
+    *   https://en.m.wikipedia.org/wiki/Caesar_cipher
+
+    Doctests
+    ========
+
+    >>> encrypt('The quick brown fox jumps over the lazy dog', 8)
+    'bpm yCqks jzwEv nwF rCuxA wDmz Bpm tiHG lwo'
+
+    >>> encrypt('A very large key', 8000)
+    's nWjq dSjYW cWq'
+
+    >>> encrypt('a lowercase alphabet', 5, 'abcdefghijklmnopqrstuvwxyz')
+    'f qtbjwhfxj fqumfgjy'
+
+    """
+    # Set default alphabet to lower and upper case english chars
+    alpha = alphabet or (ascii_uppercase + digits)
+
+    # The final result string
+    result = ""
+
+    for character in input_string:
+        if character not in alpha:
+            # Append without encryption if character is not in the alphabet
+            result += character
+        else:
+            # Get the index of the new key and make sure it isn't too large
+            new_key = (alpha.index(character) + key) % len(alpha)
+
+            # Append the encoded character to the alphabet
+            result += alpha[new_key]
+
+    return result
+
+
+def decrypt(input_string: str, key: int, alphabet: str | None = None) -> str:
+    """Decrypt.
+
+    =======
+
+    Decodes a given string of cipher-text and returns the decoded plain-text
+
+    Parameters
+    ----------
+    *   `input_string`: the cipher-text that needs to be decoded
+    *   `key`: the number of letters to shift the message backwards by to decode
+
+    Optional:
+
+    *   `alphabet` (``None``): the alphabet used to decode the cipher, if not
+        specified, the standard english alphabet with upper and lowercase
+        letters is used
+
+    Returns
+    -------
+    *   A string containing the decoded plain-text
+
+    More on the caesar cipher
+    =========================
+
+    The caesar cipher is named after Julius Caesar who used it when sending
+    secret military messages to his troops. This is a simple substitution cipher
+    where very character in the plain-text is shifted by a certain number known
+    as the "key" or "shift". Please keep in mind, here we will be focused on
+    decryption.
+
+    Example:
+    Say we have the following cipher-text:
+    ``Jgnnq, ecrvckp``
+
+    And our alphabet is made up of lower and uppercase letters:
+    ``abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ``
+
+    And our shift is ``2``
+
+    To decode the message, we would do the same thing as encoding, but in
+    reverse. The first letter, ``J`` would become ``H`` (remember: we are decoding)
+    because ``H`` is two letters in reverse (to the left) of ``J``. We would
+    continue doing this. A letter like ``a`` would shift back to the end of
+    the alphabet, and would become ``Z`` or ``Y`` and so on.
+
+    Our final message would be ``Hello, captain``
+
+    Further reading
+    ===============
+
+    *   https://en.m.wikipedia.org/wiki/Caesar_cipher
+
+    Doctests
+    ========
+
+    >>> decrypt('bpm yCqks jzwEv nwF rCuxA wDmz Bpm tiHG lwo', 8)
+    'The quick brown fox jumps over the lazy dog'
+
+    >>> decrypt('s nWjq dSjYW cWq', 8000)
+    'A very large key'
+
+    >>> decrypt('f qtbjwhfxj fqumfgjy', 5, 'abcdefghijklmnopqrstuvwxyz')
+    'a lowercase alphabet'
+
+    """
+    # Turn on decode mode by making the key negative
+    key *= -1
+
+    return encrypt(input_string, key, alphabet)
+
+
+def brute_force(input_string: str, alphabet: str | None = None) -> dict[int, str]:
+    """Brute_force.
+
+    ===========
+
+    Returns all the possible combinations of keys and the decoded strings in the
+    form of a dictionary
+
+    Parameters
+    ----------
+    *   `input_string`: the cipher-text that needs to be used during brute-force
+
+    Optional:
+
+    *   `alphabet` (``None``): the alphabet used to decode the cipher, if not
+        specified, the standard english alphabet with upper and lowercase
+        letters is used
+
+    More about brute force
+    ======================
+
+    Brute force is when a person intercepts a message or password, not knowing
+    the key and tries every single combination. This is easy with the caesar
+    cipher since there are only all the letters in the alphabet. The more
+    complex the cipher, the larger amount of time it will take to do brute force
+
+    Ex:
+    Say we have a ``5`` letter alphabet (``abcde``), for simplicity and we intercepted
+    the following message: ``dbc``,
+    we could then just write out every combination:
+    ``ecd``... and so on, until we reach a combination that makes sense:
+    ``cab``
+
+    Further reading
+    ===============
+
+    *   https://en.wikipedia.org/wiki/Brute_force
+
+    Doctests
+    ========
+
+    >>> brute_force("jFyuMy xIH'N vLONy zILwy Gy!")[20]
+    "Please don't brute force me!"
+
+    >>> brute_force(1)
+    Traceback (most recent call last):
+    TypeError: 'int' object is not iterable
+
+    """
+    # Set default alphabet to lower and upper case english chars
+    alpha = alphabet or (ascii_uppercase + digits)
+
+    # To store data on all the combinations
+    brute_force_data: dict[int, str] = {}
+
+    # Cycle through each combination
+    for key in range(1, len(alpha) + 1):
+        # Decrypt the message and store the result in the data
+        brute_force_data[key] = decrypt(input_string, key, alpha)
+
+    return brute_force_data
+
+
+def brute_force_dict_search(
+        targetstr: str,
+        datadict: dict[int, str],
+) -> None:
+    """Brute_force_dict_search.
+
+    :param targetstr: Description
+    :type targetstr: str
+    :param datadict: Description
+    :type datadict: dict
+
+    the function searches for the target string in the data dictionary
+    and prints the key and value of any matches found, including perfect matches,
+    case-insensitive matches, and partial matches. If no matches are found,
+    it prints a message indicating that no matches were found.
+    """
+    found_count = 0
+    targetstrlist = targetstr.split(",")
+    for key, value in datadict.items():
+        for targetstring in targetstrlist:
+            if targetstring.strip() == value:
+                print(
+                    "Found Perfect Match!", f"Key: {key} | Match: '{value}'", sep = "\n"
+                )
+                found_count += 1
+            elif targetstring.strip().lower() == value.lower():
+                print(f"Found Case Insensitive Match!\nKey: {key} | Match: '{value}'")
+                found_count += 1
+            elif targetstring.strip() in value:
+                print(f"Found Partial Match!\nKey: {key} | Match: '{value}' ")
+                found_count += 1
+            elif targetstring.strip().lower() in value.lower():
+                print(
+                    f"Found Case Insensitive Partial Match!\nKey: {key} | Match: '{value}'"
+                )
+                found_count += 1
+    if found_count == 0:
+        print("No Matches Found :(( ...")
+
+
+if __name__ == "__main__":
+    while True:
+        print(f"\n{'-' * 10}\n Menu\n{'-' * 10}")
+        print(*["1.Encrypt", "2.Decrypt", "3.BruteForce", "4.Quit"], sep = "\n")
+
+        choice = input("\nWhat would you like to do?: ").strip() or "4"
+
+        match choice:
+            case "1":
+                input_string = input("Please enter the string to be encrypted: ")
+                key = int(input("Please enter off-set: ").strip())
+                print(encrypt(input_string, key))
+            case "2":
+                input_string = input("Please enter the string to be decrypted: ")
+                key = int(input("Please enter off-set: ").strip())
+                print(decrypt(input_string, key))
+            case "3":
+                input_string = input("Please enter the string to be decrypted: ")
+                brute_force_data = brute_force(input_string)
+                for key, value in brute_force_data.items():
+                    print(f"Key: {key} | Message: {value}")
+                while True:
+                    targetstr = input(
+                        "Enter Target Strings To Search For Seperated By Commas If, or Enter If None:  ").strip() or "none"  # noqa: E501
+                    if targetstr.strip().lower() != "none":
+                        brute_force_dict_search(targetstr, brute_force_data)
+                    else:
+                        break
+            case "4":
+                print("Goodbye.")
+                break
+            case _:
+                print("Invalid choice, please enter a valid choice")
